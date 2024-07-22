cd8f91fcf44690265e71e3521508089a311ef536
tselimovic2 <115942763+tselimovic2@users.noreply.github.com>
2024-07-22 21:21:57 +0200

    Changed EvaluazeExpression method

--- CalculatorImpl.cs
+++ CalculatorImpl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,21 +11,21 @@ using NCalc;
 public class CalculatorImpl
 {
     private string Expression;
-    private double Result;
+    private string Result;
     private const double MAX_RESULT = 4.0;
 
-    public double ResultP
+    public string ResultP
     {
         get { return Result; }
     }
 
-    public CalculatorImpl() 
+    public CalculatorImpl()
     {
         Expression = "";
-        Result = 0;
+        Result = "0";
     }
 
-    
+
 
     public void ParseExpression(string input)
     {
@@ -41,10 +42,51 @@ public class CalculatorImpl
 
     private bool IsValidExpression(string input)
     {
-        string[] validOperators = { "+", "-", "*", "/", "%" };
+        string[] validOperators = { "+", "*", "/", "%" };
+
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == '(') count1++;
+            if (input[i] == ')') count2++;
+        }
+
+        if (count1 != count2) return false;
+
+        if (input[0] == '-' && 3 < input.Length && input[1] == ' ')
+        {
+            for (int i = 2; i < input.Length; i++)
+            {
+                char c = input[i];
+                if (!(c > '0' && c < '9') && c != 'E' && c != '(' && c != ')' && c != '+' && c != '-' && c != '*'
+                    && c != '/' && c != '%' && c != ' ' && c != '.' && c != ',')
+                {
+                    return false;
+                }
+                if (input[i] == '-')
+                {
+                    // Operator inside expression
+                    if (i > 2 && i < input.Length - 1)
+                    {
+                        if (input[i - 1] != ' ' || input[i + 1] != ' ')
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
 
         for (int i = 0; i < input.Length; i++)
         {
+            char c = input[i];
+            if (!(c > '0' && c < '9') && c != 'E' && c != '(' && c != ')' && c != '+' && c != '-' && c != '*'
+                && c != '/' && c != '%' && c != ' ' && c != '.' && c != ',')
+            {
+                return false;
+            }
             foreach (string op in validOperators)
             {
                 if (input[i].ToString() == op)
@@ -63,20 +105,101 @@ public class CalculatorImpl
         return true;
     }
 
+
+
     public void EvaluateExpression()
     {
-        try
+        string[] splitParts = Expression.Split(' ');
+        List<string> parts = splitParts.ToList();
+
+        for (int i = 0; i < parts.Count; i++)
         {
-            DataTable table = new DataTable();
-            Result = Convert.ToDouble(table.Compute(Expression, ""));
+            if (parts[0] == "-" && i == 0 && parts.Count > 3)
+            {
+                double num = Convert.ToDouble(parts[1]);
+                num = num * (-1);
+                parts[1] = num.ToString();
+                parts.RemoveAt(0);
+            }
+            if ((parts[i] == "*" || parts[i] == "/" || parts[i] == "%") && i - 1 >= 0 && i + 1 < parts.Count)
+            {
+                double first = Convert.ToDouble(parts[i - 1]);
+                Console.WriteLine(first);
+                double second = Convert.ToDouble(parts[i + 1]);
+                double res = 0;
+                if (parts[i] == "*") { res = first * second; }
+                else if (parts[i] == "/") { res = first / second; }
+                else if (parts[i] == "%") { res = first % second; }
+
+                List<string> newParts = new List<string>();
+                for (int j = 0; j < i - 1; j++)
+                {
+                    newParts.Add(parts[j]);
+                }
+                newParts.Add(res.ToString());
+                //int position = newParts.Count - 1;
+                for (int j = i + 2; j < parts.Count; j++)
+                {
+                    newParts.Add(parts[j]);
+                }
+
+                parts.Clear();
+                for (int j = 0; j < newParts.Count; j++)
+                {
+                    parts.Add(newParts[j]);
+                }
+                i = -1;
+            }
+        }
+
+        for (int i = 0; i < parts.Count; i++)
+        {
+            if (parts[0] == "-" && i == 0 && parts.Count > 3)
+            {
+                double num = Convert.ToDouble(parts[1]);
+                num = num * (-1);
+                parts[1] = num.ToString();
+                parts.RemoveAt(0);
+            }
+            if ((parts[i] == "+" || parts[i] == "-") && i - 1 >= 0 && i + 1 < parts.Count)
+            {
+                double first = Convert.ToDouble(parts[i - 1]);
+                double second = Convert.ToDouble(parts[i + 1]);
+                double res = 0;
+                if (parts[i] == "+") { res = first + second; }
+                else if (parts[i] == "-") { res = first - second; }
+
+                List<string> newParts = new List<string>();
+                for (int j = 0; j < i - 1; j++)
+                {
+                    newParts.Add(parts[j]);
+                }
+                newParts.Add(res.ToString());
+                //int position = newParts.Count - 1;
+                for (int j = i + 2; j < parts.Count; j++)
+                {
+                    newParts.Add(parts[j]);
+                }
+
+                parts.Clear();
+                for (int j = 0; j < newParts.Count; j++)
+                {
+                    parts.Add(newParts[j]);
+                }
+                i = -1;
+            }
+        }
+
+        //Result = Convert.ToDouble(parts[0]);
+
+
+        if (parts.Count == 1)
+        {
+            Result = parts[0];
             if (!CheckMaxResult())
             {
                 PrintResult();
             }
-        }
-        catch
-        {
-            Console.WriteLine("Error: Unable to evaluate expression");
         }
     }
 
@@ -84,7 +207,7 @@ public class CalculatorImpl
 
     public bool CheckMaxResult()
     {
-        if (Result > MAX_RESULT)
+        if (Convert.ToDouble(Result) > MAX_RESULT)
         {
             Console.WriteLine("A Suffusion of Yellow");
             return true;
@@ -98,6 +221,9 @@ public class CalculatorImpl
         {
             case "Red Button":
                 PrintYijingCharacter();
+                break;
+            case "":
+                Console.WriteLine("ERROR: Invalid expression!");
                 break;
             default:
                 EvaluateExpression();
